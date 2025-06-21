39b67c53449e9d2c890dffe3727edaeefd803e45
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2025-06-21 18:43:51 +0100

    Router file created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,15 @@
+import { BrowserRouter } from 'react-router-dom';
+import { Router } from './routes/Router';
 import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
     <>
-      <h1>Map Me If You Can</h1>
+      <BrowserRouter>
+        <h1>Map Me If You Can</h1>
+        <Router />
+      </BrowserRouter>
+
       <GlobalStyle />
     </>
   );
