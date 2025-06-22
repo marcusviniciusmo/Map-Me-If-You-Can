98745683bced5ce82ba4cf2f15bb762e21a8b214
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2025-06-22 04:53:43 +0100

    Navigation through Menu component

--- frontend/src/components/Menu/index.tsx
+++ frontend/src/components/Menu/index.tsx
@@ -1,3 +1,14 @@
+import { NavLink } from 'react-router-dom';
+import { routes } from '../../routes/routes';
+
 export function Menu() {
-  return <h1>MENU COMPONENT</h1>;
+  return (
+    <>
+      <h1>MENU COMPONENT</h1>
+      <NavLink to={routes.home}>Home</NavLink>
+      <NavLink to={routes.destinations}>Destinations</NavLink>
+      <NavLink to={routes.tips}>Tips</NavLink>
+      <NavLink to={routes.contact}>Contact</NavLink>
+    </>
+  );
 }
