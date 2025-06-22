d0df12971942dac73bb51ed3f87b650bee757eb0
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2025-06-22 04:37:05 +0100

    Contact page created

--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import { Home } from '../pages/Home';
 import { Destinations } from '../pages/Destinations';
 import { Tips } from '../pages/Tips';
+import { Contact } from '../pages/Contact';
 
 export function Router() {
   return (
@@ -9,6 +10,7 @@ export function Router() {
       <Route path="/" element={<Home />} />
       <Route path="/destinations" element={<Destinations />} />
       <Route path="/tips" element={<Tips />} />
+      <Route path="/contact" element={<Contact />} />
     </Routes>
   );
 }
