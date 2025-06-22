cb362eaf1d3c36bf2b8bd754ee2baadd3cb01f07
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2025-06-22 05:28:48 +0100

    Theme context typing definition

--- frontend/src/context/ThemeContext/index.tsx
+++ frontend/src/context/ThemeContext/index.tsx
@@ -1,18 +1,13 @@
-import { createContext, useState, type ReactNode } from 'react';
+import { createContext, useState } from 'react';
 import { ThemeProvider } from 'styled-components';
-import type { ThemeType } from '../../interfaces/ThemeSwitcher';
+import type {
+  ThemeType,
+  ThemeContextType,
+  ThemeContextProps,
+} from '../../interfaces/ThemeSwitcher';
 import { ThemesNames } from '../../enums/ThemeSwitcher';
 import { DarkTheme } from '../../styles/themes/dark';
 import { LightTheme } from '../../styles/themes/light';
-
-interface ThemeContextType {
-  theme: ThemeType;
-  toggleTheme: () => void;
-}
-
-interface ThemeContextProps {
-  children: ReactNode;
-}
 
 export const ThemeContext = createContext({} as ThemeContextType);
 
--- frontend/src/interfaces/ThemeSwitcher/index.ts
+++ frontend/src/interfaces/ThemeSwitcher/index.ts
@@ -1,7 +1,17 @@
+import type { ReactNode } from 'react';
 import 'styled-components';
 
 export interface ThemeType {
   name: string;
+}
+
+export interface ThemeContextType {
+  theme: ThemeType;
+  toggleTheme: () => void;
+}
+
+export interface ThemeContextProps {
+  children: ReactNode;
 }
 
 declare module 'styled-components' {
