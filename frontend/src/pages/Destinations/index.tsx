37ad7c1618b5886bb4317385305d2bc6e0300ef2
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2025-06-22 04:16:49 +0100

    Destinations page created

---
+++ frontend/src/pages/Destinations/index.tsx
@@ -0,0 +1,3 @@
+export function Destinations() {
+  return <h1>DESTINATIONS PAGE</h1>
+}
--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import { Home } from '../pages/Home';
+import { Destinations } from '../pages/Destinations';
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/destinations" element={<Destinations />} />
     </Routes>
   );
 }
